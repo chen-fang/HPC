d8874e4961d5364a9bef9363156d85c0e53bcf01
chen-fang <chen-fang@utulsa.edu>
2015-08-27 17:46:47 -0500

    1.1 done

--- HPC_HW_1.h
+++ HPC_HW_1.h
@@ -1,3 +1,38 @@
+// Problem 1.1
+// If n is evenly divisible by p
+int offset = n/p;
+int my_first_i = this_core.index * offset;
+int my_last_i  = my_first_i + offset;
+for( int i = my_first_i; i < my_last_i; ++i )
+{
+   int x = compute_value;
+   sum += x;
+}
+
+// If n is not evenly divisible by p
+// Explanation:
+// Assign the same workload to the first (p-1) cores.
+// Let the last core do the rest work.
+// Using 'rounding' to make workload_1 close to workload_2
+int workload_1 = rounding( n/p );
+int workload_2 = n - (p-1) * workload_1;
+int my_first_i = this_core.index * workload_1;
+int my_last_i;
+if( this_core.index != last_core )
+{
+   my_last_i = my_first_i + workload_1;
+}
+else
+{
+   my_last_i = my_first_i + workload_2;
+}
+for( int i = my_first_i; i < my_last_i; ++i )
+{
+   int x = compute_value;
+   sum += x;
+}
+
+
 // Problem 1.3
 // Assumption: the total number of core is a power of 2.
 int divisor = 2;
@@ -7,7 +42,13 @@ for( int iter = 0; iter < log2(total_core_number); ++iter )
    if( this_core.index % divisor == 0 )
    {
       int paired_core_index = this_core.index + core_difference;
+      // receive data from paired core
       this_core.value += paired_core.value;
+   }
+   else
+   {
+      int paired_core_index = this_core.index - core_difference;
+      // send data to paired core
    }
    divisor *= 2;
    core_difference *= 2;
@@ -24,7 +65,13 @@ for( int iter = 0; iter < log2(total_core_number); ++iter )
    if( this_core.index % divisor == 0 )
    {
       int paired_core_index = this_core.index ^ bitmask;
+      // receive data from paired core
       this_core.value += paired_core.value;
+   }
+   else
+   {
+      int paired_core_index = this_core.index - core_difference;
+      // send data to paired core
    }
    divisor *= 2;
    bitmask << 1;
