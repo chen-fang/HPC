e4087f1b4918f6ac117a4dff4c4f3f98d7ab5e76
chen-fang <chen-fang@utulsa.edu>
2015-09-15 16:14:32 -0500

    update

--- HPC_HW_Chap1_PartB.h
+++ HPC_HW_Chap1_PartB.h
@@ -18,22 +18,59 @@
 // It's a combination of both.
 // It's task-parallelism because in each iteration, some cores receive and sum
 // the values while some others send them.
-// It's also data-parallelism because each core carries portion of the total task
-// at the same time.
+// It's also data-parallelism because for cores that are assigned with the same kind of work,
+// each core carries portion of the total task at the same time.
 
 
 // Problem 1.8
-// [a]
-// cleaning the house
-// buy pizza
+// [a] task-parallelism
+// Preparation for the party may include:
+// -- clean the house
+// -- buy food
+// -- cook
+// Faculties can be divided into groups to deal with one the them.
 
 
-// [b]
+// [b] data-parallelism
 // Cleaning the house may include cleaning the
-// windows, tables, plates, floors etc.
-// If data-parallelism is applied, then all faculties do partial work on the same
-// type at the same time. For example, each faculty cleans N/p of all the windows.
+// windows, tables, plates, etc.
+// If data-parallelism is applied, then each faculty is equally assigned with partial work out of
+// the entire cleaning task. For example, each faculty needs to clean (1) the window, (2) tables
+// and (3) plates.
 
 // [c]
 // Faculties are divided into several groups. Each group picks one job, while
-// members within the group divide the work.
+// members within the same group divide the work equally.
+
+
+/*
+  Problem 1.9
+
+  Parallel implementation of black oil simulator
+
+  Black oil simulation is the basic form of reservoir simulation
+  with a list of assumptions that simplifies the physical problems.
+  To be specific, compared to compositional model, the phases 
+  rather than individual components are tracked to construct the 
+  governing equations in black oil model such that thermodynamic 
+  equilibrium is ignored and phase behavior can be easily described.
+  Even though the black oil model can fail due to its underlying
+  assumptions when, for instance, significant component partitioning
+  occurs during displacement, it is still the most widely used model
+  in the industry since its computational requirement is much lower
+  than its compositional counterpart. In recent years,
+  however, large scale simulation with million to billion grid
+  blocks are gaining attention in the industry, challenging the
+  efficiency of the black oil simulation.
+  In this project, a parallel black oil simulator will be implemented
+  When the system is large, around 70% of the total time is spent
+  on solving the equations in iterations. As a result, in addition
+  to the discretization and Jacobian evaluation procedures, more
+  emphasis will be put on parallelizing solvers of the system.
+  With referencing some most recently published work, the most 
+  commonly used preconditioners will be implemented. Also, newly
+  developed preconditioners that may be more suitable for HPC will
+  be coded as well. Tests will be run to compare the results to
+  conclude the effect of parallel computations in the context of
+  black oil simulation.
+*/
